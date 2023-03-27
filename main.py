319a35b72859d8a8758160ec95e4048a60565c22
mitul72 <mituldhawan2@gmail.com>
2023-03-27 10:11:11 -0600

    added boilerplate code and defined first function

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello World!!")
+# Initializing variables
+
+area = 0
+length = 0
+width = 0
+
+def computeRoomArea(room_number):
+    shape = input("Select the shape of the wall of the room from the following:\n"
+                  "Type '1' for Rectangular"
+                  "Type '2' for Square"
+                  "Type '3' for Custom")
+    if shape == "Rectangular".casefold():
+        area = length * width
+    elif shape == "Square".casefold():
+        area = length ** 2
+    elif shape == "Custom".casefold():
+        pass
+
+
+
+def computeRectangleWallsArea():
+    pass
+
+
+def calculateRectangleArea():
+    pass
+
+
+def computeSquareWallsArea():
+    pass
+
+
+def computeSquareArea():
+    pass
+
+
+def computeWindowsDoorsArea():
+    pass
+
+
+def computeCustomWallsArea():
+    pass
+
+
+def computeGallons():
+    pass
+
+
+def computePaintPrice():
+    pass
