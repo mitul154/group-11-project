d899dadfe068a07baa293515dec63024e8975127
OmarAbouSaleh <omar.abou-saleh@edu.sait.ca>
2023-03-31 14:37:16 -0700

    Added some comments

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Initializing variables
-paint_coverage = 350    # Paint coverage in square feet per gallon (ft2/gal)
-paint_cost = 42     # Cost of each gallon of paint ($CDN/gal)
-labour_cost = 0
-areas = []
-two_wall = 2
+paint_coverage = 350  # Paint coverage in square feet per gallon (ft2/gal)
+paint_cost = 42  # Cost of each gallon of paint ($CDN/gal)
+labour_cost = 0  # Cost of labour is included in the paint cost
+areas = []  # Empty list used for storing total area cost
+two_wall = 2    # Next 3 variables are used to reduce hard coding
 four_walls = 4
 factor_of_square = 2
 
@@ -29,7 +29,7 @@ def compute_room_area(room_number, current_room):
     total_gallons = compute_gallons(total_area)
     total_cost = compute_paint_price(total_gallons)
     print(f"For Room: {current_room}, the area to be painted is {total_area:.1f}: square ft and will require {total_gallons:.2f} "
-          f"gallons to paint. This will cost the customer ${total_cost:.2f}")
+        f"gallons to paint. This will cost the customer ${total_cost:.2f}")
 
 
 def compute_rectangle_walls_area():
@@ -58,8 +58,8 @@ def compute_windows_doors_area():
     areas.clear()
     number_of_windows_doors_area = int(input("How many windows and doors does the room contain?: "))
     for i in range(number_of_windows_doors_area):
-        length = int(input(f"Enter window/door length for window/door {i+1} in feet: "))
-        width = int(input(f"Enter window/door width for window/door {i+1} in feet: "))
+        length = int(input(f"Enter window/door length for window/door {i + 1} in feet: "))
+        width = int(input(f"Enter window/door width for window/door {i + 1} in feet: "))
         area = length * width
         areas.append(area)
 
@@ -67,8 +67,8 @@ def compute_windows_doors_area():
 def compute_custom_walls_area():
     number_of_walls = int(input("How many walls are there in the room: "))
     for i in range(number_of_walls):
-        height = int(input(f"Enter the height of wall {i+1} in feet: "))
-        length = int(input(f"Enter the length of wall {i+1} in feet: "))
+        height = int(input(f"Enter the height of wall {i + 1} in feet: "))
+        length = int(input(f"Enter the length of wall {i + 1} in feet: "))
         area_of_wall = height * length
         areas.append(area_of_wall)
 
@@ -89,6 +89,5 @@ print("Welcome to Shiny Paint Company for indoor painting!")
 number_of_rooms = int(input("How many Rooms do you want to paint:\n"))
 print("Thank you!")
 for i in range(number_of_rooms):
-    print(f"Room: {i+1}")
-    compute_room_area(number_of_rooms, i+1)
-
+    print(f"Room: {i + 1}")
+    compute_room_area(number_of_rooms, i + 1)
